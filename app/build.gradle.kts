cffeb1602bd8687897c57c225c6437524569602a
Mattfielder <wehttam989@gmail.com>
2025-03-24 00:09:01 -0600

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,28 +1,37 @@
+import androidx.glance.appwidget.compose
 import org.gradle.api.JavaVersion
-import java.util.Properties // Add this import
+import org.gradle.api.plugins.JavaPluginExtension
+import java.util.Properties
 
-// Load local.properties file
 val localProperties = Properties().apply {
-    load(rootProject.file("local.properties").inputStream())
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { java.util.ServiceLoader.load(it) }
+    } else {
+        println("Warning: local.properties file not found. API_KEY will not be loaded.")
+    }
 }
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" // Updated
+    id("kotlin-ksp")
 }
 
 android {
-    namespace = "com.evexdesignsx.myapplication" // Replace with your actual namespace
-    compileSdk = 35
+    namespace = "com.evexdesignsx.myapplication"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.evexdesignsx.myapplication" // Replace with your actual application ID
+        applicationId = "com.evexdesignsx.myapplication"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        buildConfigField("String", "API_KEY", "\"${localProperties.getProperty("API_KEY")}\"") // Fixed: Use getProperty
+
+        val apiKey = localProperties.getProperty("API_KEY") ?: ""
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -32,21 +41,25 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isMinifyEnabled = false
+        }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -54,7 +67,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4" // Updated to the latest version
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     packaging {
@@ -64,14 +77,97 @@ android {
     }
 }
 
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.material)
     implementation(libs.kotlin.stdlib)
     testImplementation(libs.junit)
     implementation(libs.lifecycle.runtime.ktx)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.androidx.compose)
+    debugImplementation(libs.bundles.compose.test)
+    implementation(libs.androidx.navigation.compose)
 }
 android {
-    namespace = "com.evexdesignsx.auraframefx"
+    namespace = "com.evexdesignsx.myapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.evexdesignsx.myapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        val apiKey = localProperties.getProperty("API_KEY") ?: ""
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material)
+    implementation(libs.kotlin.stdlib)
+    testImplementation(libs.junit)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.androidx.compose)
+    debugImplementation(libs.bundles.compose.test)
+    implementation(libs.androidx.navigation.compose)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,15 +4,129 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.gradle.enterprise") version "3.15.1"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs { 
+
+    }
     repositories {
         google()
         mavenCentral()
     }
+    dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     versionCatalogs {
 
     }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+    configurations.all {
+    resolutionStrategy {
+        force("com.google.guava:guava:32.1.2-jre")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs {
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }dependencies {
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.guava:guava:32.1.2-jre")
+        }dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs {
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs {
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.guava:guava:32.1.2-jre")
+        }
+    }
+}dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs {
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    configurations.all {
+        resolutionStrategy {
+            force("com.google.guava:guava:32.1.2-jre")
+        }
+    }
+    configurations.all {
+    resolutionStrategy {
+        force("com.google.guava:guava:32.1.2-jre")
+    }
+    gradleEnterprise {
+    buildScan {
+        publishAlways()
+        server = "https://ge.infinum.co"
+    }
+}dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    versionCatalogs {
+
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("com.google.guava:guava:32.1.2-jre")
+    }
+}
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        server = "https://ge.infinum.co"
+    }
+}resolutionStrategy {
+        force("com.google.guava:guava:32.1.2-jre")
+    }
+}
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        server = "https://ge.infinum.co"
+    }
+}
+
+include(":app")
+
 rootProject.name = "My Application"
